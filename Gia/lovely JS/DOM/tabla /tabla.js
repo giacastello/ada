d41ed659f0fb5d997f951bafd17d7ba032d3dc2d
Gia Castello <giacastello@gmail.com>
2017-11-22 11:56:36 -0300

    EDIT

--- Gia/lovely JS/DOM/tabla /tabla.js
+++ Gia/lovely JS/DOM/tabla /tabla.js
@@ -6,12 +6,21 @@ $(document).ready(function(){ //accedemos al doc linkeado, y cuando esté listo 
         //  var btnId = $(this).prop('id'); //creo una variable para el botón. This hace el cambio en el botón clickeado. (prop también puede estar como attr)
         // alert ('botón' + btnId);
    // });
-    $('.botonEliminar').on('click',function(){ //referencio la clase, y cuando clickee, va a ejecutar la función 
+    
+   $('.botonEliminar').on('click',function(){ //referencio la clase, y cuando clickee, va a ejecutar la función 
         //  var btnId = $(this).prop('id'); //creo una variable para el botón. This hace el cambio en el botón clickeado. (prop también puede estar como attr)
         var btnId = $(this).data('rg');// same as el de arriba comentado.
         // var trId='#registro_' + btnId;// creo una variable para todo el tr con el botón de
         $(btnId).hide();//la function hide puede recibir 2 parámetros(speed,callback)
         alert('Registro' + btnId +'eliminado');
-});
+    });
+    $('.botonEditar').on('click',function(){
+            var fila = $(this).data('rg');
+            $(fila).prop('disabled', false);
+    });
+    $('.botonEditar').on('click',function(){
+
+})
+
 
 });
