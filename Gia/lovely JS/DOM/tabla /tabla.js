7de03136844cd3f9f18309aa594f676cc96297c7
Gia Castello <giacastello@gmail.com>
2017-11-22 12:46:09 -0300

    Botón agregar <3

--- Gia/lovely JS/DOM/tabla /tabla.js
+++ Gia/lovely JS/DOM/tabla /tabla.js
@@ -7,6 +7,7 @@ $(document).ready(function(){ //accedemos al doc linkeado, y cuando esté listo 
         // alert ('botón' + btnId);
    // });
     
+        // DELETE
    $('.botonEliminar').on('click',function(){ //referencio la clase, y cuando clickee, va a ejecutar la función 
         //  var btnId = $(this).prop('id'); //creo una variable para el botón. This hace el cambio en el botón clickeado. (prop también puede estar como attr)
         var btnId = $(this).data('rg');// same as el de arriba comentado.
@@ -14,13 +15,19 @@ $(document).ready(function(){ //accedemos al doc linkeado, y cuando esté listo 
         $(btnId).hide();//la function hide puede recibir 2 parámetros(speed,callback)
         alert('Registro' + btnId +'eliminado');
     });
+
+         // EDIT
     $('.botonEditar').on('click',function(){
+            $('input').prop('disabled',true); // deshabilita a los demás inputs
             var fila = $(this).data('rg');
             $(fila).prop('disabled', false);
     });
     $('.botonEditar').on('click',function(){
+ })
 
-})
-
-
+        // ADD NEW
+    $('#add').on('click',function(){
+            var tr= '<tr id="registro_x"><td> <input disabled value="" type="text" class="rx"> </td><td> <input disabled value="" type="text" class="rx"> </td><td> <input disabled value="" type="number" class="rx"> </td><td> <button type="button" name="Editar" class="botonEditar" id="editx" data-rg=".r3">  &#9998;</button></td><td> <button type="button" name="Eliminar" class="botonEliminar" id="x" data-rg="registro_3">  &#x1F5D1; </button></td></tr>';
+    $('#tablis').append(tr);
+    });    
 });
