beb698b84d20a3e8c564b984f82099dfbfea8474
Gia Castello <giacastello@gmail.com>
2017-11-22 11:04:35 -0300

    estilos a tablis

--- Gia/lovely JS/DOM/tabla /tabla.js
+++ Gia/lovely JS/DOM/tabla /tabla.js
@@ -2,8 +2,16 @@
 
 // trabajando con JQUERY, la primera linea de código debe ser SIEMPRE doc ready:
 $(document).ready(function(){ //accedemos al doc linkeado, y cuando esté listo se corre la sig función
+        // $('.botonEliminar').on('click',function(){ //referencio la clase, y cuando clickee, va a ejecutar la función 
+        //  var btnId = $(this).prop('id'); //creo una variable para el botón. This hace el cambio en el botón clickeado. (prop también puede estar como attr)
+        // alert ('botón' + btnId);
+   // });
     $('.botonEliminar').on('click',function(){ //referencio la clase, y cuando clickee, va a ejecutar la función 
-            var btnId = $(this).prop('id'); //creo una variable para el botón. This hace el cambio en el botón clickeado. (prop también puede estar como attr)
-            alert ('botón' + btnId);
-    });
+        //  var btnId = $(this).prop('id'); //creo una variable para el botón. This hace el cambio en el botón clickeado. (prop también puede estar como attr)
+        var btnId = $(this).data('rg');// same as el de arriba comentado.
+        // var trId='#registro_' + btnId;// creo una variable para todo el tr con el botón de
+        $(btnId).hide();//la function hide puede recibir 2 parámetros(speed,callback)
+        alert('Registro' + btnId +'eliminado');
+});
+
 });
