b31ca2cbe3128d02213aedd1f2adb16c29304073
Gia Castello <giacastello@gmail.com>
2017-11-28 12:47:43 -0300

    concateneishon

--- Gia/lovely JS/JSON/jSon.js
+++ Gia/lovely JS/JSON/jSon.js
@@ -1,5 +1,11 @@
-var jdatos = { 'nombre':'Jake', 'apellido':'el Perro', 'Edad':'27', 'foto':'null', 'casado':'true'};
-console.log(jdatos);
+$(document).ready(function(){
+    var jdatos = {'nombre':'Jake', 'apellido':'el Perro', 'Edad':'27', 'foto':'null', 'casado':'true'};
+    console.log(jdatos);
+    
+    var jdatosJson = JSON.stringify(jdatos);
+    console.log(jdatosJson); 
 
-var jdatosJson = JSON.stringify(jdatos);
-console.log(jdatosJson);
+    var a = "<a href='jSon.html?dato=+ jdatosJson +'> Al doc </a>";//("")por fuera, y por dentro simples '
+    $('body').append(a);
+    
+});
