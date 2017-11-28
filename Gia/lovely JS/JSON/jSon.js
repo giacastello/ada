9c010733e4acf4780fe7120122643156078c9918
Gia Castello <giacastello@gmail.com>
2017-11-28 11:57:49 -0300

    Welcome Json

---
+++ Gia/lovely JS/JSON/jSon.js
@@ -0,0 +1,5 @@
+var jdatos = { 'nombre':'Jake', 'apellido':'el Perro', 'Edad':'27', 'foto':'null', 'casado':'true'};
+console.log(jdatos);
+
+var jdatosJson = JSON.stringify(jdatos);
+console.log(jdatosJson);
