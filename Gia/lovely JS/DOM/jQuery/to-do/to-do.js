bb7cf592a81778dab9d433eb453521e0015dcef0
Gia Castello <giacastello@gmail.com>
2017-11-28 11:45:15 -0300

    debugging to-do

--- Gia/lovely JS/DOM/jQuery/to-do/to-do.js
+++ Gia/lovely JS/DOM/jQuery/to-do/to-do.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
     $('#btn-add').on('click',function({ //uso funciones anónimas solo cuando se usan una vez. 
-        var valor = $('#new-item').val();
-        $('#lista').append('<li>+valor+<button id="btn-borrar"> Eliminar </button> </li>');
-        $('#new-item').val("");    
+        $('#new-item').val();
+        $('#lista').append('<li>'+valor+'<button id="btn-borrar"> Eliminar </button> </li>');
+        $('#new-item').val(" ");    
     });
 
     function borrar(){
         $(this).parent().remove();
     };
-});
+
  
     $(document).on('click','#btn-borrar',borrar);
 
-$(agregar) on('click') function() { 
-    var valor = $('#valorinput').val()
-};
+});
